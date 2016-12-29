9c1ff62c5985f46f333475e71933683ea5e2c3c0
Viktor Fonic <viktor.fonic@gmail.com>
2016-12-29 17:35:08 +0700

    feat(random function): Add a param number of random names to return
    If random is passed a number, it will return an array with that many random names. Otherwise it will
    
    return a single random name as a string, without the array.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 var starWarsNames = require('./starwars-names.json');
 var uniqueRandomArray = require('unique-random-array');
+var randomName = uniqueRandomArray(starWarsNames);
 
 module.exports = {
 	all: starWarsNames,
-	random: uniqueRandomArray(starWarsNames),
+	random: random,
 	first: starWarsNames[0]
+}
+
+function random(numberOfRandomNamesToReturn) {
+	if (numberOfRandomNamesToReturn) {
+		var randomNames = [];
+		for(var i = numberOfRandomNamesToReturn; i--;) {
+			randomNames.push(randomName());
+		}
+		return randomNames;
+	} else {
+		return randomName();
+	}
 }
